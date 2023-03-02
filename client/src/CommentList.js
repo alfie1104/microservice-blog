a8fa96ca13a5980271e4de18bf478a5dd84e5663
peter_alfie <alfie1104@gmail.com>
2023-03-03 08:13:05 +0900

    feat(client/commentlist): rendering comments by status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,7 +1,19 @@
 import React, { useEffect, useState } from "react";
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment is rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
